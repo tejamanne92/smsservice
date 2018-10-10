5c90a3fac94a0f37839dd14ea0c38e842f2e72b8
lethalhawk1001@gmail.com <teja@TEJAs-MacBook-Air.local>
2018-10-11 00:43:45 +0530

    updated

--- src/main/java/com/tejamanne/challenges/smsservice/SmsserviceApplication.java
+++ src/main/java/com/tejamanne/challenges/smsservice/SmsserviceApplication.java
@@ -4,12 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-public class SmsserviceApplication {
+public class SmsserviceApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SmsserviceApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SmsserviceApplication.class, args);
 	}
+
 }
